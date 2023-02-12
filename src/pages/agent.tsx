c86609cf8b102de3ff71df00331e93c0164a7151
Miguel Ángel García <92162464+mikaeltevet@users.noreply.github.com>
2023-02-11 21:07:05 -0600

    first commit

---
+++ src/pages/agent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Agent = () => {
+  return (
+    <div>agent</div>
+  )
+}
+
+export default Agent
---
+++ src/pages/home.tsx
@@ -0,0 +1,26 @@
+import { useList } from '@pankod/refine-core'
+import { Typography, Box, Stack } from '@pankod/refine-mui'
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent,
+} from 'components'
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex">
+
+      </Box>
+    </Box>
+  )
+}
+
+export default Home
